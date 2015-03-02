61cfc57799f15cc85a5edcddc8dd3c29418b2bf2
Mikel Larreategi <mlarreategi@codesyntax.com>
2015-03-02 09:07:19 +0100

    Change the way to create concatenated resources: look for the most recently compatible resource and concatenate the resource in there

--- cs/plonepatches/resourceregistries/BaseRegistry.py
+++ cs/plonepatches/resourceregistries/BaseRegistry.py
@@ -1,6 +1,9 @@
 
 def cookResources(self):
-    """Cook the stored resources."""
+    """Cook the stored resources. Patched version:
+       find the most compatible concatenated resource and
+       concatenate a resource in there.
+    """
     if self.ZCacheable_isCachingEnabled():
         self.ZCacheable_invalidate()
 
@@ -25,31 +28,35 @@ def cookResources(self):
                 continue
 
             if results:
-                previtem = results[-1]
+                stored = False
+                for previtem in reversed(results):
+                    # Is this resource compatible the previous one we used?
+                    if resource.getCookable() and previtem.getCookable() \
+                           and self.compareResources(resource, previtem):
+                        res_id = resource.getId()
+                        prev_id = previtem.getId()
+                        self.finalizeResourceMerging(resource, previtem)
 
-                # Is this resource compatible the previous one we used?
-                if resource.getCookable() and previtem.getCookable() \
-                       and self.compareResources(resource, previtem):
-                    res_id = resource.getId()
-                    prev_id = previtem.getId()
-                    self.finalizeResourceMerging(resource, previtem)
+                        # Add the original id under concatenated resources or
+                        # create a new one starting with the previous item
+                        if concatenatedResources.has_key(prev_id):
+                            concatenatedResources[prev_id].append(res_id)
+                        else:
+                            magic_id = self.generateId(resource, previtem)
+                            concatenatedResources[magic_id] = [prev_id, res_id]
+                            previtem._setId(magic_id)
 
-                    # Add the original id under concatenated resources or
-                    # create a new one starting with the previous item
-                    if concatenatedResources.has_key(prev_id):
-                        concatenatedResources[prev_id].append(res_id)
-                    else:
-                        magic_id = self.generateId(resource, previtem)
-                        concatenatedResources[magic_id] = [prev_id, res_id]
-                        previtem._setId(magic_id)
+                        stored = True
+                        break
 
-                else:
+                if not stored:
                     if resource.getCookable() or resource.getCacheable():
                         magic_id = self.generateId(resource)
                         concatenatedResources[magic_id] = [resource.getId()]
                         resource._setId(magic_id)
                     results.append(resource)
-            else: # No resources collated yet
+
+            else:  # No resources collated yet
 
                 # If cookable or cacheable, generate a magic id, change
                 # the resource id to be this id, and record the old id in the
@@ -59,6 +66,8 @@ def cookResources(self):
                     concatenatedResources[magic_id] = [resource.getId()]
                     resource._setId(magic_id)
                 results.append(resource)
+            # import pdb; pdb.set_trace()
+            # a = 1
 
         # Get the raw list of resources and store these as well in
         # concatenated resources
@@ -66,4 +75,6 @@ def cookResources(self):
         for resource in resources:
             concatenatedResources[resource.getId()] = [resource.getId()]
 
+        # self.cookedResourcesByTheme[theme] = tuple(anonymousResources +
+        #     memberResources + otherResources)
         self.cookedResourcesByTheme[theme] = tuple(results)
